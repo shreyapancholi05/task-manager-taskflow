24286f39ebaa6fa5589a320bd93e245d4ca71cd4
Shreya <shreyapancholi04@gmail.com>
2026-05-05 17:52:34 +0530

    backend issue fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,39 +2,50 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-
 const connectDB = require("./config/db");
+
 
 const authRoutes = require("./routes/authRoutes");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
+
 const app = express();
 
 
-connectDB();
-
-
-app.use(cors({
-  origin: [
-  "http://localhost:5173",
-    "https://task-manager-taskflow-1.onrender.com"
-],
-credentials: true
-}));
 app.use(express.json());
-const dashboardRoutes = require("./routes/dashboardRoutes");
 
-// Routes
-app.use("/api/dashboard", dashboardRoutes);
+
+app.use(
+  cors({
+    origin: "https://task-manager-taskflow-1.onrender.com",
+    credentials: true,
+  })
+);
+
+
 app.use("/api/auth", authRoutes);
-app.use("/api/projects", projectRoutes)
-app.use("/api/tasks", taskRoutes)
-app.get("/", (req, res) => {  
-  res.send("API Running");
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("API Running 🚀");
 });
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
-});
+connectDB()
+  .then(() => {
+    console.log(" MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(" Database connection failed:", err);
+    process.exit(1);
+  });
